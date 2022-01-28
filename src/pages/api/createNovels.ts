32dda858afd6900d00eeefb950f2f6167e8af5e1
Miki Nagao <m-nagao@clinks.jp>
2022-01-28 10:29:01 +0900

    amplify 初期化

--- src/pages/api/createNovels.ts
+++ src/pages/api/createNovels.ts
@@ -1,15 +1,17 @@
 import { Novels } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import { prisma } from './prisma';
 
-const handleCreate = async (req: NextApiRequest, res: NextApiResponse<Novels>) => {
-  const { title, summary, content, count } = req.body;
+// const handleCreate = async (req: NextApiRequest, res: NextApiResponse<Novels>) => {
+//   const { title, summary, content, count } = req.body;
 
-  const newNovel = await prisma.novels.create({
-    data: { title, summary, content, count },
-  });
+//   const newNovel = await prisma.novels.create({
+//     data: { title, summary, content, count },
+//   });
 
-  res.json(newNovel);
-};
+//   res.json(newNovel);
+// };
 
-export default handleCreate;
+// export default handleCreate;
+
