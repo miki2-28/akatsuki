be98ea1005a2d7e7f54e72f9b59d07f670595d4c
Miki Nagao <m-nagao@clinks.jp>
2022-02-16 16:32:12 +0900

    API を作り直した

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,12 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "function": {
-        "akatsuki7f542344": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
     "api": {
         "akatsuki": {
             "GraphQLAPIIdOutput": "string",
